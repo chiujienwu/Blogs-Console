f6b36b5e97134df1776cd80b704cb59d3e878fa7
chiujienwu <jchiu1@my.wctc.edu>
2019-10-23 09:01:53 -0500

    Initial
    new project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blogs_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
